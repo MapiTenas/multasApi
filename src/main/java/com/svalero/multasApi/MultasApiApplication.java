c3e8c8337183bc2d41d42e4d9c7412e8e3b2c3df
Mapi <mapitenar@hotmail.com>
2024-04-27 11:50:20 +0200

    Initialize

---
+++ src/main/java/com/svalero/multasApi/MultasApiApplication.java
@@ -0,0 +1,13 @@
+package com.svalero.multasApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultasApiApplication.class, args);
+	}
+
+}
